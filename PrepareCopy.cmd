c2602336e676ccabe9b085328e016c08fef0f43c
Jens Henske <67688157+JtheK1984@users.noreply.github.com>
2025-02-06 15:08:31 +0100

    Version 1.6

--- PrepareCopy.cmd
+++ PrepareCopy.cmd
@@ -1,10 +1,3 @@
 echo "Kopiere Datei ins Setupverzeichnis"
 copy /y /v Win32\Release\PCMService.exe "e:\Inno\Setupfiles\Programme\PCMService"
 copy /y /v Win64\Release\PCMService.exe "e:\Inno\Setupfiles\Programme\PCMService_x64"
-
-copy /y /v Win64\Release\PCMService.DE "e:\Inno\Setupfiles\Programme\PCMService_x64"
-copy /y /v Win64\Release\PCMService.EN "e:\Inno\Setupfiles\Programme\PCMService_x64"
-
-copy /y /v PCMService.docx "e:\Inno\Setupfiles\Programme\PCMService"
-copy /y /v PCMService.pdf "e:\Inno\Setupfiles\Programme\PCMService"
-copy /y /v PCMService.htm "e:\Inno\Setupfiles\Programme\PCMService"
