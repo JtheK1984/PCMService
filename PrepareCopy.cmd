d5c7414b198272ace20bd588df767f07c5826b45
Jens Henske <67688157+JtheK1984@users.noreply.github.com>
2025-07-28 13:15:15 +0200

    Version 1.0

--- PrepareCopy.cmd
+++ PrepareCopy.cmd
@@ -1,3 +1,9 @@
-echo "Kopiere Datei ins Setupverzeichnis"
+echo "Kopiere Datei ins Setupverzeichnis 32-Bit"
 copy /y /v Win32\Release\PCMService.exe "e:\Inno\Setupfiles\Programme\PCMService"
+echo "Kopiere Datei ins Setupverzeichnis 64-Bit"
 copy /y /v Win64\Release\PCMService.exe "e:\Inno\Setupfiles\Programme\PCMService_x64"
+
+echo "Kopiere Doku ins Setupverzeichnis"
+copy /y /v PCMService.docx "e:\Inno\Setupfiles\Programme\PCMService"
+copy /y /v PCMService.pdf "e:\Inno\Setupfiles\Programme\PCMService"
+copy /y /v PCMService.htm "e:\Inno\Setupfiles\Programme\PCMService"
--- README.md
+++ README.md
@@ -34,4 +34,4 @@
   - PrepareCopy.cmd (erzeugte Versionen werden in das Inno-Setupverzeichnis abgelgt)
 
 # Stand:
-  23.07.2025
+  28.07.2025
